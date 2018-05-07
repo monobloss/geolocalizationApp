9c977f1de74b294290fa8def97f4d66cc7882247
Daniel Glejzner <daniel.glejzner@ormlondon.com>
2018-05-07 09:19:20 +0200

    add working unit test got geo-location-service

--- src/app/core/geo-location.service.spec.ts
+++ src/app/core/geo-location.service.spec.ts
@@ -1,9 +1,12 @@
-import {TestBed, inject, ComponentFixture} from '@angular/core/testing';
+import {TestBed, getTestBed} from '@angular/core/testing';
 
 import {GeoLocationService} from "./geo-location.service";
 import {HttpClient, HttpClientModule, HttpHandler} from "@angular/common/http";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {IGeoLocationApiResponse} from "../store/interfaces/IGeoLocationApiResponse";
+let injector: TestBed;
+let geolocationservice: GeoLocationService;
+let httpMock: HttpTestingController;
 
 describe('GeoLocationService', () => {
   beforeEach(() => {
@@ -11,10 +14,27 @@ describe('GeoLocationService', () => {
       imports: [HttpClientTestingModule],
       providers: [GeoLocationService, HttpClientModule]
     });
+    injector = getTestBed();
+    geolocationservice = injector.get(GeoLocationService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('GeoLocationService should be created', () => {
     expect(GeoLocationService).toBeDefined();
+
+  });
+  it('should call get method', () => {
+
+    geolocationservice.fetchGeoLocation('google.com').subscribe((geoLocationInfo: IGeoLocationApiResponse) => {
+      expect(geoLocationInfo.ip).toBe('2607:f8b0:405:80a::200e');
+    });
+
+    const req = httpMock.expectOne('http://api.ipstack.com/google.com?access_key=0d5fd0f40d64d2aa780679cfc11d578b');
+    expect(req.request.method).toBe("GET");
 
   });
 });
